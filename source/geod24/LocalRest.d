289a5f4fd8402520832bae863e0967ea2bfd9268
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-05-27 14:30:24 +0900

    Wait for ctor forever

--- source/geod24/LocalRest.d
+++ source/geod24/LocalRest.d
@@ -106,6 +106,11 @@ import std.traits : Parameters, ReturnType;
 import core.thread;
 import core.time;
 
+/// Send by the child to its parent when the node constructor finished
+private struct CtorComplete
+{
+
+}
 
 /// Data sent by the caller
 private struct Command
@@ -387,7 +392,9 @@ public final class RemoteAPI (API, alias S = VibeJSONSerializer!()) : API
         CtorParams!Impl args, Duration timeout = Duration.init,
         string file = __FILE__, int line = __LINE__)
     {
-        auto childTid = C.spawn(&spawned!(Impl), file, line, args);
+        import std.stdio;
+        auto childTid = C.spawn(&spawned!(Impl), C.thisTid(), file, line, args);
+        C.receiveOnly!CtorComplete(C.thisTid());
         return new RemoteAPI(childTid, timeout);
     }
 
@@ -470,6 +477,7 @@ public final class RemoteAPI (API, alias S = VibeJSONSerializer!()) : API
        Params:
            Implementation = Type of the implementation to instantiate
            self = The channel on which to "listen" to receive new "connections"
+           parent = the parent thread which spawned us
            file = Path to the file that spawned this node
            line = Line number in the `file` that spawned this node
            cargs = Arguments to `Implementation`'s constructor
@@ -477,9 +485,10 @@ public final class RemoteAPI (API, alias S = VibeJSONSerializer!()) : API
     ***************************************************************************/
 
     private static void spawned (Implementation) (
-        C.Tid self, string file, int line, CtorParams!Implementation cargs)
+        C.Tid self, C.Tid parent, string file, int line, CtorParams!Implementation cargs)
         nothrow
     {
+        import std.stdio;
         import std.datetime.systime : Clock, SysTime;
         import std.algorithm : each;
         import std.range;
@@ -535,7 +544,9 @@ public final class RemoteAPI (API, alias S = VibeJSONSerializer!()) : API
 
         void runNode ()
         {
+            import std.stdio;
             scope node = new Implementation(cargs);
+            C.send(parent, CtorComplete.init);
             scheduler = new LocalScheduler;
 
             void handle (T)(T arg)
